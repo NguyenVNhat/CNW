319cbc7862a4ec124b015778b452396afd2193be
Trần Sĩ  Tiến <106511449+TranSiTien@users.noreply.github.com>
2023-12-04 09:18:09 +0700

    Update AdminDao.java

--- src/main/java/cnw/Admin/Models/Dao/AdminDao.java
+++ src/main/java/cnw/Admin/Models/Dao/AdminDao.java
@@ -3,16 +3,9 @@ package cnw.Admin.Models.Dao;
 import java.sql.*;
 
 public class AdminDao {
-    private Connection connectDB() throws ClassNotFoundException, SQLException {
-        Class.forName("com.mysql.jdbc.Driver");
-        String DB_URL = "jdbc:mysql://localhost:3306/cnw";
-        String USER_NAME = "root";
-        String PASSWORD = "";
-        Connection conn = DriverManager.getConnection(DB_URL, USER_NAME, PASSWORD);
-        return conn;
-    }
+    private Connector connector = new Connector();
     public Boolean isExitAdmin(String adminname,String password) throws SQLException, ClassNotFoundException {
-        Connection connection = connectDB();
+        Connection connection = connector.connectDB();
         Statement sttm = connection.createStatement();
         String query = "select * from ADMIN where Adminname = '"+adminname+"' and Password = '"+password+"'";
         ResultSet resultSet = sttm.executeQuery(query);
